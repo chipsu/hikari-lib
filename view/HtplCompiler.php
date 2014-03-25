e471b77a589b55b381fcd30dfd9524b4af4b0743
chips <chips@metrica.se>
2014-03-25 22:55:34 +0100

    htpl testing

--- view/HtplCompiler.php
+++ view/HtplCompiler.php
@@ -11,15 +11,16 @@ class HtplCompiler {
 
     function compile($source) {
         return json_encode([
-            ['tag' => 'h1', 'attr' => ['class' => 'header'], 'content' => '<u>Title: $title</u>'],
-            ['tag' => 'pre', 'content' => $source],
+            ['type' => 'data', 'value' => ['title' => 'Default title', 'items' => ['hello', 'world']]],
+            ['tag' => 'h1', 'attr' => ['class' => 'header'], 'content' => '<u>Title: $("title")</u>'],
+            ['tag' => 'pre', 'content' => 'Content'],
             ['tag' => 'ul', 'children' => [
-                ['type' => 'generator', 'content' => 'for($i=0; $i<5; ++$i)', 'children' =>  [
+                ['expression' => 'for($i=0; $i<5; ++$i)', 'children' =>  [
                     ['tag' => 'li', 'content' => '$i'],
-                ],
-                ['type' => 'generator', 'content' => 'foreach($items as $i)', 'children' =>  [
+                ]],
+                ['expression' => 'foreach($items as $i)', 'children' =>  [
                     ['tag' => 'li', 'content' => 'Item: @($i,"<b>test</b>")'],
-                ],
+                ]],
             ]],
         ]);
     }
@@ -34,14 +35,43 @@ class JtplCompiler {
 
     function compile($source) {
         $result = [];
-        $data = json_decode($source, true);
+        $data = is_array($source) ? $source : json_decode($source, true);
         $html = new \hikari\html\Html;
         foreach($data as $key => $value) {
+            $type = isset($value['type']) ? $value['type'] : null;
+            if($type == 'data') {
+                $result[] = '<?php';
+                foreach($value['value'] as $k => $v) {
+                    $result[] = sprintf('${%s} = %s;', var_export($k, true), var_export($v, true));
+                }
+                $result[] = '?>';
+                continue;
+            }
+            $tag = isset($value['tag']) ? $value['tag'] : null;
             $attr = isset($value['attr']) ? $value['attr'] : [];
             $content = isset($value['content']) ? $value['content'] : null;
-            $result[] = $html->tag($value['tag'], $attr, $content);
+            if(isset($value['expression'])) {
+                $result[] = sprintf('<?php %s { ?>', $value['expression']);
+            } else if($tag) {
+                $result[] = $html->open($tag, $attr);
+            }
+            if(isset($value['children'])) {
+                $result[] = $this->compile($value['children']);
+            } else if($content !== null) {
+                $result[] = $this->interpolate($content);
+            }
+            if(isset($value['expression'])) {
+                $result[] = '<?php } ?>';
+            } else if($tag) {
+                $result[] = $html->close($tag);
+            }
         }
         return implode(PHP_EOL, $result);
+    }
+
+    function interpolate($string) {
+        $string = str_replace(['@(', '$(', ')'], ['<?php echo $this->escape(', '<?php echo $this->get(', ')?>'], $string);
+        return $string;
     }
 
 }
