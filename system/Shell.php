7f79e38dfb5372817ea2a1b599052c036b87641c
chips <chips@metrica.se>
2014-04-08 20:15:24 +0200

    shell tmp fix

--- system/Shell.php
+++ system/Shell.php
@@ -12,6 +12,7 @@ class Shell extends Component {
         $command = escapeshellcmd($command);
         $args = array_map('escapeshellarg', $args);
         $exec = $command . ' ' . implode(' ', $args) . ' 2>&1';
+        $exec = '/bin/bash -c ' . escapeshellarg($exec);
         exec($exec, $this->output, $this->result);
         return $this->result == 0;
     }
