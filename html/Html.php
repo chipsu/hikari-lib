bd6af7e1ace3c56223681858004feca0d915ce11
chips <chips@metrica.se>
2014-04-14 23:48:33 +0200

    html class

--- html/Html.php
+++ html/Html.php
@@ -3,15 +3,47 @@
 namespace hikari\html;
 
 class Html extends \hikari\component\Component {
+    public static $voidElements = [
+        'area' => true,
+        'base' => true,
+        'br' => true,
+        'col' => true,
+        'embed' => true,
+        'hr' => true,
+        'img' => true,
+        'input' => true,
+        'keygen' => true,
+        'link' => true,
+        'menuitem' => true,
+        'meta' => true,
+        'param' => true,
+        'source' => true,
+        'track' => true,
+        'wbr' => true,
+    ];
+    public static $booleanAttributes = [
+        'checked' => true,
+        'compact' => true,
+        'declare' => true,
+        'defer' => true,
+        'disabled' => true,
+        'ismap' => true,
+        'multiple' => true,
+        'nohref' => true,
+        'noresize' => true,
+        'noshade' => true,
+        'nowrap' => true,
+        'readonly' => true,
+        'selected' => true,
+    ];
 
-    // TODO: special cases..
     function attributes(array $attributes) {    
         $result = [];
-        $special = ['checked' => 1, 'disabled' => 1, 'readonly' => 1, 'selected' => 1];
         foreach($attributes as $key => $value) {
-            if(isset($special[$key])) {
-                if($value)
+            if(isset(static::$booleanAttributes[$key])) {
+                if($value) {
                     $result[] = htmlspecialchars($key);
+                }
             } else if($value !== null) {
                 $result[] = sprintf('%s="%s"', htmlspecialchars($key), htmlspecialchars($value));
             }
@@ -20,7 +52,11 @@ class Html extends \hikari\component\Component {
     }
 
     function tag($name, array $attributes = [], $content = null) {
-        return $this->open($name, $attributes) . $content . $this->close($name);
+        $close = '';
+        if($content !== null || !isset(static::$voidElements[$name])) {
+            $close = $content . $this->close($name);
+        }
+        return $this->open($name, $attributes) . $close;
     }
 
     function open($name, array $attributes = []) {
