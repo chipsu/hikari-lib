40b57b64e467651696813d36ac3054fcfaa5eb83
chips <chips@metrica.se>
2014-05-26 23:19:44 +0200

    html menu

---
+++ html/Menu.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace hikari\html;
+
+class Menu extends \hikari\component\Component {
+    public $html;
+
+    function render($view, array $items, $wrap = ['nav', 'li'], $tag = 'li') {
+        $result = '';
+        foreach($wrap as $t) {
+            $result .= $this->html->open($t);
+        }
+        foreach($items as $item) {
+            # todo: item template/callback
+            $content = sprintf('<i class="fa fa-fw %s"></i> %s', $item['icon'], $item['title']);
+            $result .= $this->html->open($tag, isset($item['attr']) ? $item['attr'] : []);
+            $result .= $this->html->a(call_user_func_array(array($view, 'url'), $item['route']), [], $content);
+            $result .= $this->html->close($tag);
+        }
+        foreach($wrap as $t) {
+            $result .= $this->html->close($t);
+        }
+        return $result;
+    }
+}
