f879eb9e0a23b27742d214606b2dc6fddb54c322
chips <chips@metrica.se>
2014-11-04 20:18:16 +0100

    unittests

---
+++ tests/unit/view/HtplTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace view;
+
+use \hikari\view\View;
+
+class HtplTest extends \Codeception\TestCase\Test {
+    use \Codeception\Specify;
+
+    protected function _before() {
+    }
+
+    protected function _after() {
+    }
+
+    public function testBasic() {
+        $this->specify('h1 compile', function() {
+            $view = new View([
+                'paths' => [ __DIR__ ],
+                'compilers' => ['htpl' => '\hikari\view\compiler\Htpl2Compiler'],
+                'storage' => sys_get_temp_dir(),
+            ]);
+            $view->initialize();
+            $source = $view->find('templates/test');
+            $result = $view->template('templates/test');
+            $this->assertEquals(file_get_contents(preg_replace('/\.htpl$/', '.html', $source)), $result);
+        });
+    }
+}
